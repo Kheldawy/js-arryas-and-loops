80aeea3821984d5d293be40c5d90cd1de18d439b
Khaled Ibrahim <134504520+Kheldawy@users.noreply.github.com>
2024-11-05 14:56:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,99 +1,125 @@
-// const names = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
-// console.log(names[3]); // Prints: Maradona , Because indexing starts at 0.
+//#1
+const names = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
+console.log(names[3]); 
 
-// const newLength = names.push("Drogba");
+//#2
+const newLength = names.push("Drogba");
 
-// console.log(names); // Prints: ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo", "Drogba"]
-// console.log(newLength); // Prints: 7 , the new length of the array
+console.log(names); 
+console.log(newLength); 
 
-// const names = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
-// const newLength = names.unshift("Figo");
+//#3
+const names = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
+const newLength = names.unshift("Figo");
 
-// console.log(names); //  updated array
-// console.log(newLength); //  return value
+console.log(names); 
+console.log(newLength); 
 
-// const removedName = names.pop();
-// console.log(removedName); //Removed name
-// console.log(names); //Updated array
+//#4
+const removedName = names.pop();
+console.log(removedName); //Removed name
+console.log(names); //Updated array
 
-// const names = [
-//     "Figo",
-//     "Zlatan",
-//     "Beckham",
-//     "Zidane",
-//     "Maradona",
-//     "Pele",
-//     "Ronaldo",
-//   ];
+//#5
+const names = [
+    "Figo",
+    "Zlatan",
+    "Beckham",
+    "Zidane",
+    "Maradona",
+    "Pele",
+    "Ronaldo",
+  ];
   
-//   const lastElement = names.pop();  // Remove last element 
-//   names.unshift(lastElement);   // Beginning of the array
-//   console.log(names);
+  const lastElement = names.pop();  // Remove last element 
+  names.unshift(lastElement);   // Beginning of the array
+  console.log(names);
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
-// const result = players.includes("Messi");
+//#6
 
-// console.log(result); 
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const result = players.includes("Messi");
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+console.log(result); 
 
-// if (players.includes("Zlatan")) {
-//     console.log("Zlatan är med i listan!");
-// } else {
-//     console.log("Zlatan är inte med i listan.");
-// }
+//#7
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
 
+if (players.includes("Zlatan")) {
+    console.log("Zlatan är med i listan!");
+} else {
+    console.log("Zlatan är inte med i listan.");
+}
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
-// const index = players.indexOf("Maradona");
+//#8
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const index = players.indexOf("Maradona");
 
-// console.log(index); // "Maradona" has index 5
+console.log(index); // "Maradona" has index 5
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
-// const messiIndex = players.indexOf("Messi");
+//#9
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const messiIndex = players.indexOf("Messi");
 
-// console.log(messiIndex); // returns -1 when the element is not in the array
+console.log(messiIndex); // returns -1 when the element is not in the array
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
-// const playerString = players.join(", ");
+//#10
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const playerString = players.join(", ");
 
-// console.log(playerString);
+console.log(playerString);
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
-// const playerString = players.join(" - ");
+//#11
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const playerString = players.join(" - ");
 
-// console.log(playerString);
+console.log(playerString);
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
-// const removedPlayers = players.splice(1, 2);
+//#12
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const removedPlayers = players.splice(1, 2);
 
-// console.log(players); // Updated players array
-// console.log(removedPlayers); // Removed players
+console.log(players); // Updated players array
+console.log(removedPlayers); // Removed players
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
-// const result = players.splice(2, 0, "Ronaldinho");
+//#13
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const result = players.splice(2, 0, "Ronaldinho");
 
-// console.log(players); // "Ronaldinho" is added at index position 2.
-// console.log(result); // Returns an empty array []
+console.log(players); // "Ronaldinho" is added at index position 2.
+console.log(result); // Returns an empty array []
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+//#14
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
 
-// const ronaldoIndex = players.indexOf("Ronaldo");
+const ronaldoIndex = players.indexOf("Ronaldo");
 
-// if (ronaldoIndex !== -1) {
-//     players.splice(ronaldoIndex, 1);
-// }
+if (ronaldoIndex !== -1) {
+    players.splice(ronaldoIndex, 1);
+}
 
-// console.log(players);
+console.log(players);
 
-// const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
-// const firstThreePlayers = players.slice(0, 3);
+//#15
+const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
+const firstThreePlayers = players.slice(0, 3);
 
-// console.log(players);
-// console.log(firstThreePlayers); // slice() does not modify the original array
+console.log(players);
+console.log(firstThreePlayers); // slice() does not modify the original array
 
+//#16
 const players = ["Zlatan", "Beckham", "Zidane", "Ronaldo", "Romario", "Maradona"];
 const newPlayers = players.with(1, "Mbappé");
 
 console.log(newPlayers);
 console.log(players);
+
+//#17
+const players = ["Zlatan", "Beckham", "Zidane", "Maradona", "Pele", "Ronaldo"];
+
+const playersFromIndex2 = names.slice(2);
+
+console.log(playersFromIndex2);
+console.log(players); 
+
+
